425f6945f24948b5bbc83a8abe261a734af9b1bf
Rois <rois.khoiron@gmail.com>
2021-04-23 14:01:05 +0700

    create komponen navbar dengan jsx pada App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div>
+      <h3>Navbar</h3>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h2>Hello World</h2>
     </div>
   );
