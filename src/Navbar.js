1811b7b77049df7b68c63d421739bb99a0c114d9
Rois <rois.khoiron@gmail.com>
2021-04-23 14:12:17 +0700

    jenis component berdasarkan jenis object returnya

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+// react function component - cal('rfc')
 export default function Navbar() {
   return (
     <div>
@@ -7,3 +8,29 @@ export default function Navbar() {
     </div>
   )
 }
+
+// react class component - cal('rcc')
+/* import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Navbar</h2>
+      </div>
+    )
+  }
+} */
+
+// react arrow function component - call ('rafc')
+/* import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div>
+      <h2>Navbar</h2>
+    </div>
+  )
+} */
+
+
