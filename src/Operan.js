94d2bc7ceba7256948e491bec094198e762391bb
Rois <rois.khoiron@gmail.com>
2021-04-28 09:44:42 +0700

    simplifikasi props elemen (variable, function, dll) kedalam variable

--- src/Operan.js
+++ src/Operan.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react'
 
 export default class Operan extends Component {
   render() {
-    return (
+    const { makanan, setNewFood } = this.props
+
+    return (        
       <div>
-        <h2>Makana yg diterima dari state dan diterima oleh props ini : {this.props.makanan}</h2>
-        <button onClick={() => this.props.setNewFood('Mie Ayam')}>Ganti Makanan</button>
+        <h2>Makana yg diterima dari state dan diterima oleh props ini : {makanan}</h2>
+        <button onClick={() => setNewFood('Mie Ayam')}>Ganti Makanan</button>
       </div>
     )
   }
